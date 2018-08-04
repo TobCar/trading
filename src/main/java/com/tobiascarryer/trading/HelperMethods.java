e940b0ade31fd576d35014ed7d2e7994be794fb9
TobCar <TobCar@users.noreply.github.com>
2018-08-03 19:47:04 -0700

    Add chooseFile helper method

--- src/main/java/com/tobiascarryer/trading/HelperMethods.java
+++ src/main/java/com/tobiascarryer/trading/HelperMethods.java
@@ -1,6 +1,9 @@
 package com.tobiascarryer.trading;
 
+import java.io.File;
 import java.math.BigDecimal;
+
+import javax.swing.JFileChooser;
 
 public class HelperMethods {
 	public static BigDecimal roundToNearestPointFive( BigDecimal toRound ) {
@@ -8,4 +11,15 @@ public class HelperMethods {
 		// / 2 returns the rounded number to .5 instead of 1
 		return new BigDecimal(Math.round(toRound.doubleValue() * 2.0) / 2.0);
 	}
+	
+	public static File chooseFile(){
+        JFileChooser fc = new JFileChooser();
+        File file = null;
+        int returnVal = fc.showOpenDialog(null);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            file = fc.getSelectedFile();  
+        } 
+        return file;
+    }
 }
