85ffa23d8cebc19d43b13246e8e7b560ee13bf5a
TobCar <TobCar@users.noreply.github.com>
2018-08-03 21:41:48 -0700

    Centralize hyperparameters

---
+++ src/main/java/com/tobiascarryer/trading/models/sequentialprobabilities/SequentialProbabilitiesHyperparameters.java
@@ -0,0 +1,7 @@
+package com.tobiascarryer.trading.models.sequentialprobabilities;
+
+public class SequentialProbabilitiesHyperparameters {
+	public static int maxBinsInSequence = 5;
+	public static int minBinsInSequence = 2;
+	public static int numberOfBinIntervals = 4; // Must run precalculated parameters again if this is changed
+}
