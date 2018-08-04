9335cd34550c7612ebfd14e8b2083056a1c9be08
TobCar <TobCar@users.noreply.github.com>
2018-08-04 10:55:54 -0700

    Add BinSequence

---
+++ src/main/java/com/tobiascarryer/trading/models/sequentialprobabilities/BinSequence.java
@@ -0,0 +1,39 @@
+package com.tobiascarryer.trading.models.sequentialprobabilities;
+
+public class BinSequence {
+	
+	private PercentageChangeBin[] bins;
+	
+	public BinSequence(PercentageChangeBin[] bins) {
+		this.bins = bins;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if( other == null )
+            return false;
+
+        if( !BinSequence.class.isAssignableFrom(other.getClass()) )
+            return false;
+
+        BinSequence otherSequence = (BinSequence) other;
+        
+        if( bins.length != otherSequence.bins.length )
+        	return false;
+        
+        for( int i = 0; i < bins.length; i++ ) {
+        	if( !otherSequence.bins[i].equals(bins[i]) )
+        		return false;
+        }
+        	
+        return true;
+	}
+	
+	@Override
+	public String toString() {
+		String toReturn = "";
+		for( PercentageChangeBin bin: bins )
+			toReturn += bin.toString();
+		return toReturn;
+	}
+}
---
+++ src/test/java/com/tobiascarryer/trading/unittests/models/BinSequenceTest.java
@@ -0,0 +1,47 @@
+package com.tobiascarryer.trading.unittests.models;
+
+import static org.junit.Assert.*;
+
+import com.tobiascarryer.trading.models.sequentialprobabilities.BinSequence;
+import com.tobiascarryer.trading.models.sequentialprobabilities.PercentageChangeBin;
+import org.junit.Test;
+
+public class BinSequenceTest {
+
+	@Test
+	public void testEquality() {
+		PercentageChangeBin bin1 = new PercentageChangeBin(1);
+		PercentageChangeBin bin2 = new PercentageChangeBin(2);
+		PercentageChangeBin bin3 = new PercentageChangeBin(3);
+		
+		PercentageChangeBin[] bins = {bin1, bin2, bin3};
+		BinSequence sequence = new BinSequence(bins);
+		
+		PercentageChangeBin equalBin1 = new PercentageChangeBin(1);
+		PercentageChangeBin equalBin2 = new PercentageChangeBin(2);
+		PercentageChangeBin equalBin3 = new PercentageChangeBin(3);
+		
+		PercentageChangeBin[] equalBins = {equalBin1, equalBin2, equalBin3};
+		BinSequence equalSequence = new BinSequence(equalBins);
+		
+		PercentageChangeBin[] notEqualBins = {bin1, bin2};
+		BinSequence notEqualSequence = new BinSequence(notEqualBins);
+		
+		assertEquals(sequence, sequence);
+		assertEquals(sequence, equalSequence);
+		assertNotEquals(sequence, notEqualSequence);
+		assertNotEquals(sequence, null);
+	}
+	
+	@Test
+	public void testToString() {
+		PercentageChangeBin bin1 = new PercentageChangeBin(1);
+		PercentageChangeBin bin2 = new PercentageChangeBin(-2);
+		PercentageChangeBin bin3 = new PercentageChangeBin(3);
+		
+		PercentageChangeBin[] bins = {bin1, bin2, bin3};
+		BinSequence sequence = new BinSequence(bins);
+		
+		assertEquals("1-23", sequence.toString());
+	}
+}
