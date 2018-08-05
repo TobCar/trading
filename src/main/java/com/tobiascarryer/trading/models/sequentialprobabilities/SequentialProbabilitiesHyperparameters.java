b094eea6ab912bb95e0ff422296b49b41a0bfdb8
TobCar <TobCar@users.noreply.github.com>
2018-08-04 17:31:22 -0700

    Add hyperparameters for filtering through predictions

--- src/main/java/com/tobiascarryer/trading/models/sequentialprobabilities/SequentialProbabilitiesHyperparameters.java
+++ src/main/java/com/tobiascarryer/trading/models/sequentialprobabilities/SequentialProbabilitiesHyperparameters.java
@@ -4,4 +4,6 @@ public class SequentialProbabilitiesHyperparameters {
 	public static int maxBinsInSequence = 5;
 	public static int minBinsInSequence = 2;
 	public static int numberOfBinIntervals = 4; // Must run precalculated parameters again if this is changed
+	public static double minimumConfidence = 0.75;
+	public static double minimumTimesPatternsWereEncountered = 4;
 }
